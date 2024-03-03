a334b0a8aadadfe21817baa37614320e4eb75059
Kartik <kartik20044@gmail.com>
2024-03-03 04:21:46 -0300

    dynamic routes added

---
+++ basicroutes/src/app/products/[productId]/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+const ProductDetails = ({params}) => {
+    
+  return (
+    <div>
+      <h1>Details of product {params.productId} </h1>
+    </div>
+  )
+}
+
+export default ProductDetails
---
+++ basicroutes/src/app/products/page.jsx
@@ -0,0 +1,15 @@
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Products
